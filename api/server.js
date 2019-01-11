a8675cc9b22996f4b1531ea8889d1d13cdedc0f2
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-11 12:25:21 -0500

    Added actions. All CRUD except create are working

--- api/server.js
+++ api/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const server = express();
 
 const projectDb = require('../data/helpers/projectModel');
+const actionDb = require('../data/helpers/actionModel');
+const configureMiddleware = require('../config/middleware');
+
+configureMiddleware(server);
 
 // Projects
 server.get('/api/projects', async (req, res) => {
@@ -10,7 +14,7 @@ server.get('/api/projects', async (req, res) => {
     res.status(200).json(projects);
   } catch (error) {
     res.status(400).json({
-      errorMessage: 'Unable to get users.'
+      errorMessage: 'Unable to get projects.'
     });
   }
 });
@@ -20,9 +24,9 @@ server.get('/api/projects/:id', async (req, res) => {
     const { id } = req.params;
     const projects = await projectDb.get(id);
     projects === undefined
-      ? res
-          .status(404)
-          .json({ message: 'The user with the specified ID does not exist.' })
+      ? res.status(404).json({
+          message: 'The project with the specified ID does not exist.'
+        })
       : res.status(200).json(projects);
   } catch (error) {
     res.status(400).json({
@@ -66,14 +70,99 @@ server.delete('/api/projects/delete/:id', async (req, res) => {
     const count = await projectDb.remove(req.params.id);
     if (count === 0) {
       res.status(404).json({
-        message: 'The user with this ID does not exist.'
+        message: 'The project with this ID does not exist.'
       });
     } else {
-      res.status(200).json({ message: 'The user has been removed.' });
+      res.status(200).json({ message: 'The project has been removed.' });
     }
   } catch (error) {
     console.log('err is', error);
-    res.status(500).json({ message: 'The user could not be removed.' });
+    res.status(500).json({ message: 'The project could not be removed.' });
+  }
+});
+
+// Actions
+server.get('/api/actions', async (req, res) => {
+  try {
+    const actions = await actionDb.get();
+    res.status(200).json(actions);
+  } catch (error) {
+    res.status(400).json({
+      errorMessage: 'Unable to get actions.'
+    });
+  }
+});
+
+server.get('/api/actions/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const actions = await actionDb.get(id);
+    actions === undefined
+      ? res.status(404).json({
+          message: 'The action with the specified ID does not exist.'
+        })
+      : res.status(200).json(actions);
+  } catch (error) {
+    res.status(400).json({
+      errorMessage: 'Unable to get actions.'
+    });
+  }
+});
+
+// server.post('/api/actions', async (req, res) => {
+//   try {
+//     const action = req.body;
+//     const actionId = await actionDb.insert(action);
+//     res.status(201).json(actionId);
+//   } catch (error) {
+//     res.status(400).json({
+//       errorMessage:
+//         'There was an error while saving the action to the database.'
+//     });
+//   }
+// });
+
+server.post('/api/actions', async (req, res) => {
+  try {
+    const actions = req.body;
+    const actionInfo = await actionDb.insert(actions);
+    res.status(201).json(actionInfo);
+  } catch (error) {
+    console.log('error from post', error);
+    res.status(400).json({
+      errorMessage: 'There was an error while saving the post to the database.'
+    });
+  }
+});
+
+server.put('/api/actions/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const changes = req.body;
+    const count = await actionDb.update(id, changes);
+    res.status(201).json(count);
+  } catch (error) {
+    console.log('error from put', error);
+    res.status(400).json({
+      errorMessage:
+        'There was an error while updating the action to the database.'
+    });
+  }
+});
+
+server.delete('/api/actions/:id', async (req, res) => {
+  try {
+    const count = await actionDb.remove(req.params.id);
+    if (count === 0) {
+      res.status(404).json({
+        message: 'The action with this ID does not exist.'
+      });
+    } else {
+      res.status(200).json({ message: 'The action has been removed.' });
+    }
+  } catch (error) {
+    console.log('err is', error);
+    res.status(500).json({ message: 'The action could not be removed.' });
   }
 });
 
